0234a18f93ea1d7ce47cdb79c3a9b4e2099b8d43
Akhmetshin <renat.new@gmail.com>
2022-02-18 17:52:20 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Title = "Training Calculator";
+
+            //Console.WriteLine("Hello World!\n");
+            //Console.BackgroundColor = ConsoleColor.Green;
+            //Console.WriteLine("Hello World!\n");
+            //Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello World! My brilliant idea. Version: -1\n");
+
+            Random rnd = new Random();
+            int a, b; // c, d, e... ets
+            int res;
+            int count = 0;
+            int level = 1;
+            char oper = '+';
+            char operation1 = '+';
+            char operation2 = '-';
+            char operation3 = '*';
+            char operation4 = '/';
+
+            // https://docs.microsoft.com/ru-ru/dotnet/api/system.console.readkey?view=netframework-4.7.2&f1url=%3FappId%3DDev16IDEF1%26l%3DRU-RU%26k%3Dk(System.Console.ReadKey)
+            ConsoleKeyInfo cki;
+            Console.CursorTop = 20;
+            do
+            {
+                a = rnd.Next(1, 99);
+                b = rnd.Next(1, 99);
+                switch (oper)
+                {
+                    case '+':res = a + b;
+                        break;
+                    default:
+                }
+                Console.CursorTop = 5;
+
+                cki = Console.ReadKey(true);
+                if (cki.Key >= ConsoleKey.D0 && cki.Key <= ConsoleKey.D9)
+                {
+                    Console.Beep();
+                    Console.Write(((int)cki.Key - 48).ToString());
+                    Console.CursorLeft = 0;
+                }
+                else
+                {
+                    Console.CursorTop = 20;
+                    Console.Write(" --- You pressed ");
+                    if ((cki.Modifiers & ConsoleModifiers.Alt) != 0) Console.Write("ALT+");
+                    if ((cki.Modifiers & ConsoleModifiers.Shift) != 0) Console.Write("SHIFT+");
+                    if ((cki.Modifiers & ConsoleModifiers.Control) != 0) Console.Write("CTL+");
+                    //Console.Write(cki.Key.ToString());
+                    Console.CursorLeft = 0;
+                }
+            } while (cki.Key != ConsoleKey.Escape && cki.Key != ConsoleKey.Q);
+        }
+    }
+}
---
+++ Project1/Project1.cpp
@@ -0,0 +1,84 @@
+﻿// Project1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    HANDLE hnd = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO     cursorInfo;
+    GetConsoleCursorInfo(hnd, &cursorInfo);
+    //cursorInfo.bVisible = false;
+    //SetConsoleCursorInfo(hnd, &cursorInfo);
+    SetConsoleTextAttribute(hnd, 0xc);
+    SetConsoleOutputCP(1251);
+
+    CONSOLE_FONT_INFOEX cfi_old;
+    cfi_old.cbSize = sizeof(cfi_old);
+    GetCurrentConsoleFontEx(hnd, FALSE, &cfi_old);
+
+    CONSOLE_FONT_INFOEX cfi;
+    cfi.cbSize = sizeof(cfi);
+    cfi.nFont = 0;
+    cfi.dwFontSize.X = 0;                   // Width of each character in the font
+    cfi.dwFontSize.Y = 22;                  // Height
+    cfi.FontFamily = FF_DONTCARE;
+    cfi.FontWeight = FW_NORMAL;
+    wcscpy_s(cfi.FaceName, L"Consolas"); // Choose your font
+    SetCurrentConsoleFontEx(hnd, FALSE, &cfi);
+
+    std::cout << "Hello World!\n";
+
+    // https://stackoverflow.com/questions/4053837/colorizing-text-in-the-console-with-c
+    //for (int k = 1; k < 255; k++)
+    //{
+    //    // pick the colorattribute k you want
+    //    SetConsoleTextAttribute(hnd, k);
+    //    std::cout << k << " I want to be nice today!" << std::endl;
+    //}
+    
+    HWND wnd = GetConsoleWindow();
+
+    // https://docs.microsoft.com/en-us/windows/win32/winmsg/using-messages-and-message-queues
+    MSG msg;
+    BOOL bRet;
+    while ((bRet = GetMessage(&msg, GetConsoleWindow(), 0, 0)) != 0)
+    {
+        if (msg.message == VK_ESCAPE)
+        {
+            break;
+        }
+        if (msg.wParam == WM_KEYDOWN)
+        {
+            break;
+        }
+        //switch (msg.message)
+        //{
+        //case WM_KEYDOWN:
+        //}
+        if (bRet == -1)
+        {
+            // handle the error and possibly exit
+        }
+        else
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    } 
+
+    int c;
+    do
+    {
+        c = getchar();
+//        if (c == 'z') break;
+        int x = c;
+    }while (c != 'z');
+
+    printf("\n\n\tКонец обработки.\n\n");
+
+    Sleep(1500);
+
+    SetCurrentConsoleFontEx(hnd, FALSE, &cfi_old);
+}
